d2c2d98df6765fa892440dd95a02bfcdfdf58b27
Jilna Jk <jilnajk@gmail.com>
2025-08-13 17:32:20 +0200

    ecs error fix

--- infra/deploy/ecs.tf
+++ infra/deploy/ecs.tf
@@ -1,49 +1,40 @@
 ##
 # ECS Cluster for running app on Fargate.
 ##
-
 resource "aws_iam_policy" "task_execution_role_policy" {
   name        = "${local.prefix}-task-exec-role-policy"
   path        = "/"
   description = "Allow ECS to retrieve images and add to logs."
   policy      = file("./templates/ecs/task-execution-role-policy.json")
 }
-
 resource "aws_iam_role" "task_execution_role" {
   name               = "${local.prefix}-task-execution-role"
   assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
 }
-
 resource "aws_iam_role_policy_attachment" "task_execution_role" {
   role       = aws_iam_role.task_execution_role.name
   policy_arn = aws_iam_policy.task_execution_role_policy.arn
 }
-
 resource "aws_iam_role" "app_task" {
   name               = "${local.prefix}-app-task"
   assume_role_policy = file("./templates/ecs/task-assume-role-policy.json")
 }
-
 resource "aws_iam_policy" "task_ssm_policy" {
   name        = "${local.prefix}-task-ssm-role-policy"
   path        = "/"
   description = "Policy to allow System Manager to execute in container"
   policy      = file("./templates/ecs/task-ssm-policy.json")
 }
-
 resource "aws_iam_role_policy_attachment" "task_ssm_policy" {
   role       = aws_iam_role.app_task.name
   policy_arn = aws_iam_policy.task_ssm_policy.arn
 }
-
 resource "aws_cloudwatch_log_group" "ecs_task_logs" {
   name = "${local.prefix}-api"
 }
-
 resource "aws_ecs_cluster" "main" {
   name = "${local.prefix}-cluster"
 }
-
 resource "aws_ecs_task_definition" "api" {
   family                   = "${local.prefix}-api"
   requires_compatibilities = ["FARGATE"]
@@ -52,7 +43,6 @@ resource "aws_ecs_task_definition" "api" {
   memory                   = 512
   execution_role_arn       = aws_iam_role.task_execution_role.arn
   task_role_arn            = aws_iam_role.app_task.arn
-
   container_definitions = jsonencode(
     [
       {
@@ -92,11 +82,6 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = false
             containerPath = "/vol/web/static"
             sourceVolume  = "static"
-          },
-          {
-            readOnly      = false
-            containerPath = "/vol/web/media"
-            sourceVolume  = "efs-media"
           }
         ],
         logConfiguration = {
@@ -131,11 +116,6 @@ resource "aws_ecs_task_definition" "api" {
             readOnly      = true
             containerPath = "/vol/static"
             sourceVolume  = "static"
-          },
-          {
-            readOnly      = true
-            containerPath = "/vol/media"
-            sourceVolume  = "efs-media"
           }
         ]
         logConfiguration = {
@@ -149,35 +129,18 @@ resource "aws_ecs_task_definition" "api" {
       }
     ]
   )
-
   volume {
     name = "static"
   }
-
-  volume {
-    name = "efs-media"
-    efs_volume_configuration {
-      file_system_id     = aws_efs_file_system.media.id
-      transit_encryption = "ENABLED"
-
-      authorization_config {
-        access_point_id = aws_efs_access_point.media.id
-        iam             = "DISABLED"
-      }
-    }
-  }
-
   runtime_platform {
     operating_system_family = "LINUX"
     cpu_architecture        = "X86_64"
   }
 }
-
 resource "aws_security_group" "ecs_service" {
   description = "Access rules for the ECS service."
   name        = "${local.prefix}-ecs-service"
   vpc_id      = aws_vpc.main.id
-
   # Outbound access to endpoints
   egress {
     from_port   = 443
@@ -185,7 +148,6 @@ resource "aws_security_group" "ecs_service" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   # RDS connectivity
   egress {
     from_port = 5432
@@ -196,26 +158,12 @@ resource "aws_security_group" "ecs_service" {
       aws_subnet.private_b.cidr_block,
     ]
   }
-
-  # NFS Port for EFS volumes
-  egress {
-    from_port = 2049
-    to_port   = 2049
-    protocol  = "tcp"
-    cidr_blocks = [
-      aws_subnet.private_a.cidr_block,
-      aws_subnet.private_b.cidr_block,
-    ]
-  }
-
   # HTTP inbound access
   ingress {
-    from_port = 8000
-    to_port   = 8000
-    protocol  = "tcp"
-    security_groups = [
-      aws_security_group.lb.id
-    ]
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
@@ -229,18 +177,13 @@ resource "aws_ecs_service" "api" {
   enable_execute_command = true
 
   network_configuration {
+    assign_public_ip = true
+
     subnets = [
-      aws_subnet.private_a.id,
-      aws_subnet.private_b.id
+      aws_subnet.public_a.id,
+      aws_subnet.public_b.id
     ]
 
     security_groups = [aws_security_group.ecs_service.id]
   }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.api.arn
-    container_name   = "proxy"
-    container_port   = 8000
-  }
 }
-
